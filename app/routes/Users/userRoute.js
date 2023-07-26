a206a2af44b13471c39ba223d144560e68e9079a
Ameen Butt <ameenullahkhalid@mtechub.org>
2023-07-26 18:13:32 +0500

    resetpassword updated and other small changes have been made

--- app/routes/Users/userRoute.js
+++ app/routes/Users/userRoute.js
@@ -8,4 +8,5 @@ userRouter.post("/sign-in-user", userController.signInUser);
 userRouter.get("/get-user-data",auth, userController.getUserData);
 userRouter.post("/forget-password", userController.forgetPassword);
 userRouter.get("/otp-verification", userController.otpVerification);
+userRouter.post("/reset-password", userController.resetPassword);
 module.exports =  userRouter;
